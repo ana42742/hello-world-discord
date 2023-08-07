31918a05c63154b7d723465178444ce0274c1d5e
ana42742 <rebellajoy@gmail.com>
2023-08-07 12:46:43 +0530

    Add basic QandAs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,4 @@
-use discord_flows::{
-    model::Message,
-    ProvidedBot, Bot,
-};
+use discord_flows::{model::Message, Bot, ProvidedBot};
 use flowsnet_platform_sdk::logger;
 
 #[no_mangle]
@@ -27,12 +24,54 @@ async fn handler(bot: &ProvidedBot, msg: Message) {
     }
 
     let channel_id = msg.channel_id;
-    let resp = format!("Welcome to flows.network.\nYou just said: '{}'.\nLearn more at: https://github.com/flows-network/hello-world\n", msg.content);
+    let user_message = msg.content.to_lowercase();
 
-    _ = discord.send_message(
-        channel_id.into(),
-        &serde_json::json!({
-            "content": resp
-        }),
-    ).await;
+    if user_message.contains("hello") {
+        let response = "Hello there! How can I help you today?";
+        send_response_to_discord(&discord, channel_id, response).await;
+    } else if user_message.contains("explain image segmentation") {
+        let response = "Image segmentation is a computer vision task that divides an image into meaningful regions,
+        grouping pixels with similar characteristics. It plays a vital role in object recognition and scene analysis.
+        Popular methods include region-based, edge-based, and deep learning-based approaches, such as U-Net and Mask R-CNN.
+        (Reference: R. C. Gonzalez and R. E. Woods, Digital Image Processing, 3rd Edition, 2007.)";
+        send_response_to_discord(&discord, channel_id, response).await;
+    } else if user_message.contains("ffmpeg") {
+        let response = "FFmpeg is a powerful open-source multimedia framework that facilitates audio and video processing,
+        encoding, decoding, transcoding, and streaming. It supports a wide range of formats and codecs, making it a versatile
+        tool for media manipulation. (Reference: FFmpeg official website - https://ffmpeg.org/about.html)";
+        send_response_to_discord(&discord, channel_id, response).await;
+    } else if user_message.contains("WasmEdge") {
+        let response = "WasmEdge is an open-source, high-performance runtime for WebAssembly (Wasm) designed for edge
+        computing and the Internet of Things (IoT). It executes Wasm modules securely and efficiently, enabling seamless
+        integration of multiple programming languages. (Reference: WasmEdge GitHub repository - https://github.com/WasmEdge/WasmEdge)";
+        send_response_to_discord(&discord, channel_id, response).await;
+    } else if user_message.contains("object detection") {
+        let response = "An object detection image processing pipeline involves several steps. First, preprocess the image
+        by resizing and normalizing it. Then, extract relevant features through techniques like edge detection. Next, propose
+        potential object regions using methods such as selective search. Extract features from these regions and classify them
+        using a model like a Convolutional Neural Network (CNN). Apply non-maximum suppression to eliminate overlapping detections.
+        Refine the results through post-processing, filtering out false positives. Finally, visualize the detected objects by
+        drawing bounding boxes. Modern methods integrate these steps into deep learning networks for more accurate and efficient
+        object detection, such as YOLO and Faster R-CNN.";
+        send_response_to_discord(&discord, channel_id, response).await;
+    } else if user_message.contains("bye") {
+        // Respond to "bye" message
+        let response = "Good-bye!";
+        send_response_to_discord(&discord, channel_id, response).await;
+    } else {
+        // Respond to any other message
+        let response = "I'm sorry, I didn't understand your question.";
+        send_response_to_discord(&discord, channel_id, response).await;
+    }
+
+    // let resp = format!("Welcome to flows.network.\nYou just said: '{}'.\nLearn more at: https://github.com/flows-network/hello-world\n", msg.content);
+
+    _ = discord
+        .send_message(
+            channel_id.into(),
+            &serde_json::json!({
+                "content": response
+            }),
+        )
+        .await;
 }
